9d3ad9b83426d5dd37916ca5ec0c7ee77e6c3a6b
ognn <ogunozalp1991@gmail.com>
2023-03-06 01:47:52 +0300

    first commit

---
+++ src/main/java/org/example/packet/packet_201.scala
@@ -0,0 +1,17 @@
+package org.example.packet
+
+case class TETKIKSONUCU(`@value`: String, `@self-closing`: String)
+case class ISLEMREFERANSNUMARASI(`@value`: String, `@self-closing`: String)
+case class ISTEMZAMANI(`@value`: String, `@self-closing`: String)
+case class ISTEMYAPILANKLINIK(`@version`: String, `@codeSystemGuid`: String, `@code`: String, `@value`: String, `@self-closing`: String)
+case class ISTEMYAPANHEKIMKIMLIKNUMARASI(`@value`: String, `@self-closing`: String)
+case class RAPORLAMAZAMANI(`@value`: String, `@self-closing`: String)
+case class RAPORLAYANHEKIMKIMLIKNUMARASI(`@value`: String, `@self-closing`: String)
+case class NUMUNENINALINDIGIDOKUNUNTEMELOZELLIGI(`@version`: String, `@codeSystemGuid`: String, `@code`: String, `@value`: String, `@self-closing`: String)
+case class NUMUNEALINMASEKLI(`@version`: String, `@codeSystemGuid`: String, `@code`: String, `@value`: String, `@self-closing`: String)
+case class PREPARATDURUMU(`@version`: String, `@codeSystemGuid`: String, `@code`: String, `@value`: String, `@self-closing`: String)
+case class YERLESIMYERI(`@version`: String, `@codeSystemGuid`: String, `@code`: String, `@value`: String, `@self-closing`: String)
+case class MORFOLOJIKODU(`@version`: String, `@codeSystemGuid`: String, `@code`: String, `@value`: String, `@self-closing`: String)
+case class PATOLOJIBASE64PDFBILGISI(`@value`: String, `@self-closing`: String)
+case class PATOLOJIBILGISI(TETKIK_SONUCU: TETKIKSONUCU, ISLEM_REFERANS_NUMARASI: ISLEMREFERANSNUMARASI, ISTEM_ZAMANI: ISTEMZAMANI, ISTEM_YAPILAN_KLINIK: ISTEMYAPILANKLINIK, ISTEM_YAPAN_HEKIM_KIMLIK_NUMARASI: ISTEMYAPANHEKIMKIMLIKNUMARASI, RAPORLAMA_ZAMANI: RAPORLAMAZAMANI, RAPORLAYAN_HEKIM_KIMLIK_NUMARASI: RAPORLAYANHEKIMKIMLIKNUMARASI, NUMUNENIN_ALINDIGI_DOKUNUN_TEMEL_OZELLIGI: NUMUNENINALINDIGIDOKUNUNTEMELOZELLIGI, NUMUNE_ALINMA_SEKLI: NUMUNEALINMASEKLI, PREPARAT_DURUMU: PREPARATDURUMU, YERLESIM_YERI: YERLESIMYERI, MORFOLOJI_KODU: MORFOLOJIKODU, PATOLOJI_BASE_64_PDF_BILGISI: PATOLOJIBASE64PDFBILGISI)
+case class HASTAPATOLOJIBILGILERI(PATOLOJI_BILGISI: Vector[PATOLOJIBILGISI])
