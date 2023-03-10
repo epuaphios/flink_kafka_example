5e3c097f9bc5f71e63b980a9165addc1bafb1cae
ognn <ogunozalp1991@gmail.com>
2023-03-10 14:35:45 +0300

    kudu

---
+++ src/main/java/org/example/parser/Helper.java
@@ -0,0 +1,114 @@
+package org.example.parser;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Helper {
+
+    public static HashMap<Class, Method> valueGetters = new HashMap<>();
+    public Map<String, Integer> sequenceIds = new HashMap<>();
+
+    public static String getValue(Object object) {
+        try {
+            Class cls = object.getClass();
+            if (!valueGetters.containsKey(cls)) {
+                valueGetters.put(cls, object.getClass().getMethod("getValue"));
+            }
+            Method valueGetter = valueGetters.get(cls);
+            return (String) valueGetter.invoke(object);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            if (object instanceof String) {
+                return (String) object;
+            }
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static Long getDatetimeValue(Object object) {
+        String stringValue = getStringValue(object);
+        if (stringValue != null && !stringValue.trim().equals("")) {
+            return DateUtil.toImpalaCompatibleUnixTime(stringValue);
+        }
+        return null;
+    }
+
+    public static String getStringValue(Object object) {
+        // TODO: Move to global or use global if exists
+        if (object != null) {
+            String value = getValue(object);
+            if (value != null && !value.trim().equals("")) {
+                return value.trim();
+            }
+        }
+        return null;
+    }
+
+    public static Double getDoubleValue(Object object) {
+        // TODO: Move to global or use global if exists
+        String stringValue = getStringValue(object);
+        Double doubleValue = null;
+        if (stringValue != null) {
+            stringValue = stringValue.replaceAll(",", ".");
+            try {
+                doubleValue = Double.valueOf(stringValue).doubleValue();
+            } catch (NumberFormatException e) {
+                // Some floats are weird, nothing to do here.
+                // Null will be returned.
+            }
+        }
+        return doubleValue;
+    }
+
+    public static Integer getIntegerCode(Object object) {
+        // TODO: Move to global or use global if exists
+        if (object != null) {
+            String code = getCode(object);
+            if (code != null && !code.trim().equals("")) {
+                return Integer.valueOf(code.trim());
+            }
+        }
+        return null;
+    }
+
+    public Integer getNextId(String sequence) {
+        // CM: A great performance comparison of different ways to do the operation done below
+        // https://stackoverflow.com/questions/81346/most-efficient-way-to-increment-a-map-value-in-java
+        //should be atomic...
+        Integer id = sequenceIds.getOrDefault(sequence, -1) + 1;
+        sequenceIds.put(sequence, id);
+        return id;
+    }
+    public static Integer getIntegerValue(Object object) {
+        // TODO: Move to global or use global if exists
+        String stringValue = getStringValue(object);
+        Integer integerValue = null;
+        if (stringValue != null) {
+            // Assume that thousands separator cannot be sent, so it must be decimal separator.
+            stringValue = stringValue.replaceAll(",", ".");
+
+            // Remove decimal separator
+            int indexOfDecimalSeperator = stringValue.indexOf(".");
+            if (indexOfDecimalSeperator != -1) {
+                stringValue = stringValue.substring(0, indexOfDecimalSeperator);
+            }
+
+            try {
+                integerValue = Integer.parseInt(stringValue);
+            } catch (Exception e) {
+                integerValue = null;
+            }
+        }
+        return integerValue;
+    }
+    public static String getCode(Object object) {
+        try {
+            return (String) object.getClass().getMethod("getCode").invoke(object);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
--- src/main/java/org/example/parser/Parsers.scala
+++ src/main/java/org/example/parser/Parsers.scala
@@ -1,25 +1,17 @@
 package org.example.parser
 import org.apache.flink.types.Row
 import org.example.packet.JsonRoot
-object Parsers {
+import org.example.parser.Helper.{getDatetimeValue, getStringValue}
+
+object Parsers extends Helper {
 
    def parse201(jsonRoot: JsonRoot) = {
       val row = new Row(3)
       row.setField(0, jsonRoot.key)
       jsonRoot.content.HASTA_PATOLOJI_BILGILERI.PATOLOJI_BILGISI.forEach(
         x => {
-          if (x.ISTEM_ZAMANI == null) {
-            row.setField(1, null)
-          }
-          else {
-            row.setField(1, Some(x.ISTEM_ZAMANI).value)
-          }
-          if (x.TETKIK_SONUCU == null) {
-            row.setField(2, null)
-          }
-          else {
-            row.setField(2, x.TETKIK_SONUCU.`@value`)
-          }
+          row.setField(1,getDatetimeValue(x.ISTEM_ZAMANI))
+          row.setField(2,getStringValue(x.TETKIK_SONUCU))
         }
       )
       row
