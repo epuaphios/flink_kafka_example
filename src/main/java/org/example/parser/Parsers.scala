460490d61fe7cf9c49b550ab9b3d486b8f1e13df
ognn <ogunozalp1991@gmail.com>
2023-03-14 18:18:49 +0300

    dateutil fix

--- src/main/java/org/example/parser/Parsers.scala
+++ src/main/java/org/example/parser/Parsers.scala
@@ -1,5 +1,6 @@
 package org.example.parser
 import org.apache.flink.types.Row
+import org.apache.kudu.client.Upsert
 import org.example.packet.{JsonRoot, RADYOLOJIBILGISI}
 import org.example.parser.Helper.{getDatetimeValue, getKabulZamaniValue, getStringCode, getStringValue}
 
@@ -10,18 +11,22 @@ import scala.util.hashing.MurmurHash3
 object Parsers extends Helper {
 
    def parse201(radyolojiBilgisi: RADYOLOJIBILGISI, row: Row, index: Int) = {
-          row.setField(2, index)
-          row.setField(4,null)
-          row.setField(5,getStringValue(radyolojiBilgisi.RADYOLOJI_LOINC))
-          row.setField(6,getStringCode(radyolojiBilgisi.RADYOLOJI_LOINC))
-          row.setField(7,getStringValue(radyolojiBilgisi.ISLEM_REFERANS_NUMARASI))
-          row.setField(8,getDatetimeValue(radyolojiBilgisi.RAPOR_ONAYLANMA_ZAMANI))
-          row.setField(9,time.LocalDateTime.now())
+       val operation = new Upsert()
+       val row = operation.getRow
+
+          row.addInt(2, index)
+          row.addString(4,null)
+          row.addString(5,getStringValue(radyolojiBilgisi.RADYOLOJI_LOINC))
+          row.addString(6,getStringCode(radyolojiBilgisi.RADYOLOJI_LOINC))
+          row.addString(7,getStringValue(radyolojiBilgisi.ISLEM_REFERANS_NUMARASI))
+          row.addLong(8,getDatetimeValue(radyolojiBilgisi.RAPOR_ONAYLANMA_ZAMANI))
+          row.addLong(9,time.LocalDateTime.now().toEpochSecond(time.ZoneOffset.UTC))
           row
     }
 
   def parseCenter(jsonRoot: JsonRoot): Row = {
-    val row = new Row(10)
+    val operation = new Upsert()
+    val row = operation.getRow
     row.setField(0, jsonRoot.key)
     row.setField(1, getKabulZamaniValue(jsonRoot.kabulzamani))
     row.setField(3, murmurHash(jsonRoot.idhash))
